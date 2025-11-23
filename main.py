e5f84d550862194180378ca0105d62038c82b6d5
sonuupendra <sonuupendrabhat@gmail.com>
2025-11-23 18:35:50 +0530

    Add Changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # TODO-1: Import and print the logo from art.py when the program starts.
-import art
-print(art.logo)
+from art import logo
+print(logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
